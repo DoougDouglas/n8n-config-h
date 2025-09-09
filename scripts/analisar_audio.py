a4b95844c55bb2bce022890e29ab84d9c8a55c58
Douglas Silva <dooug_douglas@hotmail.com>
2025-09-09 11:43:20 -0300

    Update analisar_audio.py

--- scripts/analisar_audio.py
+++ scripts/analisar_audio.py
@@ -4,7 +4,6 @@ import parselmouth
 from parselmouth.praat import call
 import math
 
-# --- FUNÇÃO AUXILIAR PARA CONVERTER HERTZ EM NOTA MUSICAL ---
 def frequency_to_note(frequency):
     if not frequency or not isinstance(frequency, (int, float)) or frequency <= 0:
         return "N/A"
@@ -16,23 +15,21 @@ def frequency_to_note(frequency):
     note_index = half_steps % 12
     return f"{note_names[note_index]}{octave}"
 
-# --- SCRIPT PRINCIPAL ---
-
 filename = sys.argv[1]
 
 try:
     sound = parselmouth.Sound(filename)
     
-    # 1. ANÁLISE DE PITCH (Funciona)
     pitch = sound.to_pitch()
     mean_pitch_hz = call(pitch, "Get mean", 0, 0, "Hertz")
     pitch_note = frequency_to_note(mean_pitch_hz)
 
-    # 2. HARMONICS-TO-NOISE RATIO (HNR) (Funciona)
     harmonicity = sound.to_harmonicity()
     hnr_db = call(harmonicity, "Get mean", 0, 0)
+    
+    # --- NOVA MÉTRICA ADICIONADA ---
+    intensity_db = sound.get_intensity()
 
-    # 3. FORMANTES (Funciona)
     duration = sound.get_total_duration()
     formant = sound.to_formant_burg(time_step=0.01)
     f1_hz = call(formant, "Get value at time", 1, duration / 2, "Hertz", "Linear")
@@ -43,6 +40,7 @@ try:
         "pitch_hz": mean_pitch_hz,
         "pitch_note": pitch_note,
         "hnr_db": hnr_db,
+        "intensity_db": intensity_db, # <-- Novo dado
         "formant1_hz": f1_hz,
         "formant2_hz": f2_hz
     }
